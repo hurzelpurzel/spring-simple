89e2ac7570cde0113e4eab2022aede7857e6d138
pottmeier <ludger.pottmeier@adesso.de>
2024-09-14 23:20:47 +0200

    finnally

--- src/main/java/com/example/demo/ServingWebContentApplication.java
+++ src/main/java/com/example/demo/ServingWebContentApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class ServingWebContentApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+    return builder.sources(ServingWebContentApplication.class);
+  }
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
