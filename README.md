0503bc1b7c76bde4fb0c1905a8214fa729815181
pottmeier <ludger.pottmeier@adesso.de>
2024-09-15 16:25:29 +0200

    add stuff

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ https://www.springcloud.io/post/2022-09/spring-boot--wildfly/#gsc.tab=0
 
 # spnego
 https://www.baeldung.com/spring-security-kerberos
+
+
+# Java
+https://docs.oracle.com/javase/8/docs/technotes/guides/security/jgss/tutorials/KerberosReq.html
--- src/main/java/com/example/demo/ServingWebContentApplication.java
+++ src/main/java/com/example/demo/ServingWebContentApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,10 +10,23 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class ServingWebContentApplication extends SpringBootServletInitializer {
 
+  static {
+		System.setProperty("java.security.krb5.conf",	Paths.get("/etc/krb5.conf").normalize().toAbsolutePath().toString());
+    System.setProperty("java.security.krb5.realm","POTTMEIER.DE");
+		System.setProperty("sun.security.krb5.debug", "true");
+		// disable usage of local kerberos ticket cache
+		System.setProperty("http.use.global.creds", "false");
+	}
+  
+  
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
     return builder.sources(ServingWebContentApplication.class);
   }
+
+
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
